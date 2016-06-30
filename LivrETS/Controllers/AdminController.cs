28f8592dcbd5079f996b149d649698f459df15fc
Charles Levesque <dreamtooloverture@gmail.com>
2016-06-30 01:16:52 -0400

    chore Re-created the project There was a bug with the old project that prevented a good push to the production environment. By re-creating the project, it worked.

---
+++ LivrETS/Controllers/AdminController.cs
@@ -0,0 +1,287 @@
+﻿/*
+LivrETS - Centralized system that manages selling of pre-owned ETS goods.
+Copyright (C) 2016  TribuTerre
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using LivrETS.Models;
+using LivrETS.ViewModels;
+using System.Net;
+
+namespace LivrETS.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private ApplicationUserManager _userManager;
+
+        public AdminController() { }
+
+        public AdminController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: /Admin/ManageUsers
+        [HttpGet]
+        public ActionResult ManageUsers()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                ViewBag.users = UserManager.Users.ToList();
+                ViewBag.roles = (from role in db.Roles select role).ToList();
+            }
+            return View();
+        }
+
+        // GET: /Admin/ManageFairs
+        [HttpGet]
+        public ActionResult ManageFairs()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                ViewBag.fairs = (from fair in db.Fairs select fair).ToList();
+            }
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _userManager?.Dispose();
+                _userManager = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Ajax
+
+        // PUT: /Admin/ChangeUserRole
+        // Change the role of a user.
+        [HttpPut]
+        public ActionResult ChangeUserRole(AjaxManageUsersViewModel model)
+        {
+            var user = UserManager.FindById(model.UserId);
+            
+            if (user == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+
+            // Remove from all roles just in case.
+            UserManager.RemoveFromRoles(user.Id, UserManager.GetRoles(user.Id).ToArray());
+
+            if (model.NewRole != null)
+            {
+                UserManager.AddToRole(user.Id, model.NewRole);
+            }
+
+            return Json(new { }, contentType: "application/json");
+        }
+
+        // DELETE: /Admin/Delete
+        // Deletes one or multiple users.
+        [HttpDelete]
+        public ActionResult DeleteUser(AjaxManageUsersViewModel model)
+        {
+            List<ApplicationUser> users = new List<ApplicationUser>();
+            
+            if (model.UserId != null)  // Only one user to delete
+            {
+                users.Add(UserManager.FindById(model.UserId));
+            }
+            else  // Multiple users to delete
+            {
+                users = model.UserIdsList.ConvertAll(new Converter<string, ApplicationUser>(userId => UserManager.FindById(userId)));
+            }
+
+            foreach (var user in users)
+            {
+                if (user != null)
+                {
+                    Unregister(user);
+                }
+            }
+
+            return Json(new { }, contentType: "application/json");
+        }
+
+        // POST: /Admin/Fair
+        // Adds or modifies a new fair to the system. To modify, populate the Id when posting.
+        [HttpPost]
+        public ActionResult Fair(AjaxFairViewModel model)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                Fair fair;
+                if (model.Id == null)
+                {
+                    fair = new Fair();
+                }
+                else
+                {
+                    fair = db.Fairs.FirstOrDefault(fairDb => fairDb.Id.ToString() == model.Id);
+
+                    if (fair == null)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+                    }
+                }
+
+                fair.SetDates(model.StartDate, model.EndDate)
+                    .SetDates(model.PickingStartDate, model.PickingEndDate, forPhase: FairPhase.PICKING)
+                    .SetDates(model.SaleStartDate, model.SaleEndDate, forPhase: FairPhase.SALE)
+                    .SetDates(model.RetrievalStartDate, model.RetrievalEndDate, forPhase: FairPhase.RETRIEVAL);
+                fair.Trimester = model.Trimester;
+
+                if (model.Id == null)
+                {
+                    db.Fairs.Add(fair);
+                }
+
+                db.SaveChanges();
+            }
+
+            return Json(new { }, contentType: "application/json");
+        }
+
+        // DELETE: /Admin/DeleteFair
+        // Deletes one or more fairs.
+        [HttpDelete]
+        public ActionResult DeleteFair(AjaxFairViewModel model)
+        {
+            List<Fair> fairs = new List<Fair>();
+
+            using (var db = new ApplicationDbContext())
+            {
+                if (model.Id != null)
+                {
+                    var fair = db.Fairs.FirstOrDefault(fairDb => fairDb.Id.ToString() == model.Id);
+
+                    if (fair != null)
+                    {
+                        fairs.Add(fair);
+                    }
+                    else
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+                    }
+                }
+                else
+                {
+                    fairs = model.IdsList.ConvertAll(new Converter<string, Fair>(id => db.Fairs.FirstOrDefault(fair => fair.Id.ToString() == id)));
+
+                    if (fairs.Any(fair => fair == null))
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+                    }
+                }
+
+                foreach (var fair in fairs)
+                {
+                    db.Fairs.Remove(fair);
+                }
+                db.SaveChanges();
+            }
+            return Json(new { }, contentType: "application/json");
+        }
+
+        // POST: /Admin/GetFairData
+        // Gets the data of a fair. POST method is used to avoid verifying
+        // an anti-forgery token with GET.
+        [HttpPost]
+        public ActionResult GetFairData(AjaxFairViewModel model)
+        {
+            Fair fair = null;
+            using (var db = new ApplicationDbContext())
+            {
+                fair = (
+                    from fairdb in db.Fairs
+                    where fairdb.Id.ToString() == model.Id
+                    select fairdb
+                ).FirstOrDefault();
+            }
+
+            if (fair == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                return Json(new AjaxFairViewModel
+                {
+                    Id = fair.Id.ToString(),
+                    StartDate = fair.StartDate,
+                    EndDate = fair.EndDate,
+                    PickingStartDate = fair.PickingStartDate,
+                    PickingEndDate = fair.PickingEndDate,
+                    SaleStartDate = fair.SaleStartDate,
+                    SaleEndDate = fair.SaleEndDate,
+                    RetrievalStartDate = fair.RetrievalStartDate,
+                    RetrievalEndDate = fair.RetrievalEndDate,
+                    Trimester = fair.Trimester
+                }, contentType: "application/json");
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        /// <summary>
+        /// Removes a user from the system completely.
+        /// </summary>
+        /// <param name="user">The user to unregister</param>
+        private void Unregister(ApplicationUser user)
+        {
+            var logins = UserManager.GetLogins(user.Id);
+            var roles = UserManager.GetRoles(user.Id);
+
+            foreach (var login in logins)
+            {
+                UserManager.RemoveLogin(user.Id, login);
+            }
+
+            if (roles.Count > 0)
+            {
+                UserManager.RemoveFromRoles(user.Id, roles.ToArray());
+            }
+
+            UserManager.Delete(user);
+        }
+
+        #endregion
+    }
+}
---
+++ LivrETS/Scripts/manageusers.js
@@ -0,0 +1,154 @@
+﻿/*
+LivrETS - Centralized system that manages selling of pre-owned ETS goods.
+Copyright (C) 2016  TribuTerre
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+$(document).ready(function () {
+    $("thead>tr").find("th").addClass("text-center");
+
+    $("#close-error").on("click", function () {
+        $("#errors").hide("slow");
+    });
+
+    // Role change event.
+    $("input[type='radio'][name='UserRole']").on("change", function () {
+        $.ajax({
+            method: "PUT",
+            contentType: "application/json",
+            url: "/Admin/ChangeUserRole",
+            dataType: "json",
+            data: JSON.stringify({
+                UserId: $(this).attr("data-user-id"),
+                NewRole: $(this).val()
+            }),
+            success: function () { },
+            error: function (err) {
+                console.log(err);
+                $("#error-message").text("Une erreur est survenue lors du changement de privilège.");
+                $("#errors").show("slow");
+            }
+        });
+    });
+
+    // Select single user event.
+    $("input[type='checkbox'][name='check-select-user-for-action']").on("change", function () {
+        updateDeleteSelectedView();
+    });
+
+    // Delete user event.
+    $(".btn-delete-user").on("click", function () {
+        var button = $(this);
+        var userId = $(this).attr("data-user-id");
+
+        button.prop("disabled", true);
+        $.ajax({
+            method: "DELETE",
+            contentType: "application/json",
+            url: "/Admin/DeleteUser",
+            dataType: "json",
+            data: JSON.stringify({
+                UserId: userId
+            }),
+            success: function () {
+                button.parents("tr").remove();
+                updateDeleteSelectedView();
+            },
+            error: function () {
+                button.prop("disabled", false);
+                $("#error-message").text("Une erreur est survenue lors de la suppression de l'utilisateur.");
+                $("#errors").show("slow");
+            }
+        });
+    });
+
+    // Select all users's checkbox for action
+    $("input[type='checkbox'][name='check-select-all']").on("change", function () {
+        var checked = $(this).is(":checked");
+
+        $("tbody>tr>td")
+            .find("input[type='checkbox'][name='check-select-user-for-action']")
+            .prop("checked", checked);
+        updateDeleteSelectedView();
+    });
+
+    // Delete selected event            
+    $("#div-actions").on("click", "#div-delete-selected>#btn-delete-selected", function () {
+        $(this).prop("disabled", true);
+        var userIds = [];
+
+        $("input[type='checkbox'][name='check-select-user-for-action']:checked").each(function () {
+            userIds.push($(this).attr("data-user-id"));
+        });
+
+        $.ajax({
+            method: "DELETE",
+            contentType: "application/json",
+            url: "/Admin/DeleteUser",
+            dataType: "json",
+            data: JSON.stringify({
+                UserIds: userIds.join()
+            }),
+            success: function () {
+                $("input[type='checkbox'][name='check-select-user-for-action']:checked").each(function () {
+                    $(this).parents("tr").remove();
+                    $(this).prop("disabled", false);
+                    updateDeleteSelectedView();
+                });
+            },
+            error: function () {
+                $("#error-message").text("Une erreur est survenue lors de la suppression des l'utilisateurs. Svp, rechargez la page.");
+                $("#errors").show("slow");
+            }
+        });
+    });
+});
+
+/**
+ * Updates the selected number in the actions panel.
+ */
+function updateDeleteSelectedView() {
+    var checkedCount = $("tbody>tr>td").find("input[type='checkbox'][name='check-select-user-for-action']:checked").length;
+
+    if ($("#div-delete-selected").is(":visible")) {
+        if (checkedCount === 0) {
+            $("#div-delete-selected").remove();
+        } else {
+            $("#nb-delete-selected").text("" + checkedCount);
+        }
+    } else {
+        if (checkedCount > 0) {
+            var text =
+            $("<p>").append(  // p
+                $("<u>")  // u
+                    .text(" utilisateur(s) sélectionné(s)")
+                    .prepend($("<span>")  // span
+                        .attr("id", "nb-delete-selected")
+                        .text("" + checkedCount)
+                    )
+            );
+            var deleteButton = $("<button>")
+                .attr("id", "btn-delete-selected")
+                .attr("class", "btn btn-danger")
+                .text("Supprimer");
+            var deleteDiv = $("<div>")
+                .attr("class", "row")
+                .attr("id", "div-delete-selected")
+                .append(text)
+                .append(deleteButton);
+
+            $("#div-actions").append(deleteDiv);
+        }
+    }
+}
