32520cc9068f4896829aeb3018785c4e479b9fda
willkoua <willkoua@gmail.com>
2017-04-18 13:23:05 -0400

    upgrate fair informations for users

---
+++ LivrETS/Scripts/fair/informations.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+
+    var table = $('#table-2').DataTable({
+        processing: true,
+        ajax: {
+            url: "/Admin/ListOffersFair",
+            data: { id: $("#table-2").data("id") },
+            type: "POST",
+            dataType: "JSON",
+            dataSrc: function (val) {
+                return val.Offers
+            }
+        },
+        columns: [
+           {
+               sortable: false,
+               data: function (val) {
+
+                   return val.Title
+               }
+           },
+            {
+                data: function (val) {
+                    return parseFloat(val.Price).toFixed(2) + " $";;
+                }
+            }
+        ]
+
+    });
+
+});
